0ce5be831a9c130b08c537521518f55117cdab22
LukyKozlojc <l.kozel@outlook.cz>
2023-06-28 21:12:36 +0200

    finished part A pythonic Card Deck

---
+++ Pythonic_cardDeck.py
@@ -0,0 +1,36 @@
+import collections
+import random
+
+Card = collections.namedtuple('Card',['rank','suit'])
+
+class deck:
+    ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank,suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+    
+suit_values = dict(spades =3, hearts =2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = deck.ranks.index(card.rank)
+    return rank_value*len(suit_values)+suit_values[card.suit]
+
+
+newDeck = deck()
+print("lol")
+randomCard = newDeck.__getitem__(random.randint(0,newDeck.__len__()))
+print(randomCard)
+print(newDeck[-1])
+print(newDeck[:3])
+print (randomCard in newDeck)
+
+for card in sorted(newDeck,key=spades_high):
+    print(card)
